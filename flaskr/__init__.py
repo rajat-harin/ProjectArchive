50d8d82cca29d34f75640ea9e9d668f1c9a3e019
rajat-harin <rajat.harinkhede@gmail.com>
2023-02-02 13:07:21 +0530

    for testing Deployment try2

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import (Flask, render_template)
 from flaskr.auth import login_required
-from flaskr.config import DevelopmentConfig
+#from flaskr.config import DevelopmentConfig
 from flask_cors import CORS
 
 def create_app(test_config = None):
@@ -13,7 +13,7 @@ def create_app(test_config = None):
 
     if test_config is None:
         #load instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        #app.config.from_pyfile('config.py', silent=True)
         app.config.from_prefixed_env()
     else:
         app.config.from_mapping(test_config)
